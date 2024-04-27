3a5787e5f263e7c62dc5b0c21b0bdeb7ad7b3783
Zeynab Movasaqpoor <zeynab.mvs@gmail.com>
2024-04-27 14:32:40 +0330

    add assets, basic tailwind configs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ function App() {
 
   return (
     <>
-     <h1 className="text-3xl text-pink-500">hi</h1>
+     <h1 className="text-5xl font-dm text-green">hi</h1>
+     <h1 className="text-3xl font-mono  text-yellow">hi</h1>
+     <h1 className="text-3xl text-red">hi</h1>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dm: ['"DM Sans"', ...fontFamily.sans]
+      }
+    },
+    colors: {
+      n: {
+        1: '#1B1D1F',
+        2: '#6F757C',
+        3: '#111315',
+      },
+      green: '#BEE3CC',
+      cream: '#FEF7EE',
+      yellow: '#F6C768',
+      red: '#ED735D'
+    },
+    fontSize: {
+      heading: '2rem',
+      body: '1rem',
+      label: '0.875rem',
+      sm: '0.625rem',
+      price: '0.75rem'
+    }
   },
   plugins: [],
 };
